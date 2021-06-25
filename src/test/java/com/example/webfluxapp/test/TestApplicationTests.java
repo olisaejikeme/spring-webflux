7bf8f5855cf2ca21c890ffbe3953941fd06b19cd
Olisa Ejikeme <olisaejikeme@gmail.com>
2021-06-25 13:08:04 +0100

    student application with spring webflux

---
+++ src/test/java/com/example/webfluxapp/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxapp.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
